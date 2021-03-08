997081606e6a7d7a325064ae892aa30984137eb5
Pongsin Pongwachirint <bazio.developer@gmail.com>
2021-03-09 00:18:04 +0700

    update

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
-from core.views import home
+from django.urls import path, include
+from core.views import *
+
 
 urlpatterns = [
     path('', home, name="home"),
+    path('signup/', signup, name="signup"),
+    path('secret/', secret_page, name="secret"),
+    path('secret2', SecretPage.as_view(), name="secret2"),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
